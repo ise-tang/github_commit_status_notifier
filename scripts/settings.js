ace2d8873a755686f131d86eb177f558f634ba67
issei126 <issei.nakamura@peroli.jp>
2017-12-11 20:46:30 +0900

    save check interval

--- scripts/settings.js
+++ scripts/settings.js
@@ -5,4 +5,11 @@ document.addEventListener("DOMContentLoaded", function(){
     localStorage.setItem('token', input.value);
     document.getElementById('save_result').innerText = 'Your token has been saveed';
   })
+
+  var save_interval = document.getElementById('save_interval');
+  save_interval.addEventListener('click', function(){
+    var input = document.getElementById('interval');
+    localStorage.setItem('interval', input.value);
+    document.getElementById('save_result').innerText = 'Status check Interval has been saveed';
+  })
 });
