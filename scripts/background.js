31980347cc818530d5d7b45b2473a7a64a61d42c
issei126 <issei.nakamura@peroli.jp>
2017-12-11 18:55:01 +0900

    use page action

--- scripts/background.js
+++ scripts/background.js
@@ -1,3 +1,14 @@
+// Called when the url of a tab changes.
+function checkForValidUrl(tabId, changeInfo, tab) {
+  console.log("checkForValidUrl");
+    if (/https:\/\/github\.com\/.*\/pull\//.test(tab.url)) {
+      chrome.pageAction.show(tabId);
+    }
+  };
+
+  // Listen for any changes to the URL of any tab.
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
+
 var commitUrl = "";
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse){
