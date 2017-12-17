d839bf505241a3c13e6a14199c18dd7e94dbefb0
issei126 <issei.nakamura@peroli.jp>
2017-12-17 22:28:38 +0900

    delete console.log

--- scripts/background.js
+++ scripts/background.js
@@ -1,6 +1,5 @@
 // Called when the url of a tab changes.
 function checkForValidUrl(tabId, changeInfo, tab) {
-  console.log("checkForValidUrl");
   if (/https:\/\/github\.com\/.*\/pull\//.test(tab.url)) {
     chrome.pageAction.show(tabId);
   }
@@ -21,12 +20,10 @@ chrome.pageAction.onClicked.addListener(function(){
   // request commit url to content script
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {}, function(response) {
-      console.log(response.url);
       commitUrl = response.url;
 
       notify("Pull Request CI Status", "start checking");
 
-      console.log(commitUrl);
       var org_repo_re = /\/(.*?\/.*?)\/pull/;
       var org_repo = org_repo_re.exec(commitUrl)[1];
 
@@ -38,7 +35,6 @@ chrome.pageAction.onClicked.addListener(function(){
         var url = "https://api.github.com/repos/" + org_repo + "/commits/" + commit + "/status?access_token=" + localStorage.getItem("token");
         xhr.open("GET", url);
         xhr.onload = function(){
-          console.log(xhr.response["state"]);
           if (xhr.response["state"] != "pending"){
             notify("Pull Request CI Status", "Result: " + xhr.response["state"]);
             clearInterval(id);
